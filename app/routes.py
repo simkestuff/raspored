8e851cd5d45428963491a8c78c1dca1fc76314be
simkestuff <simkestuff@gmail.com>
2022-02-10 20:26:29 +0100

    added index.html and css

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 from . import core, database, models
 
 
 router = APIRouter()
+
+#router.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@router.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse("index.html", {'request': request})
 
 @router.get("/danas", response_model=models.DayScheduleBase)
 def today_timetable():
--- raspored.py
+++ raspored.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app import routes
 
 app = FastAPI()
 
 app.include_router(routes.router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
